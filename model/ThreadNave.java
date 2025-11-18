d693aeb428994a09dfb87400509939eddcecac25
ItaloSLeao <italoleao.dev@gmail.com>
2025-11-18 03:02:53 -0300

    feat: adiciona controle de colisao/espera

--- model/ThreadNave.java
+++ model/ThreadNave.java
@@ -3,3 +3,8 @@
 * Matricula........: 202410120
 * Inicio...........: 30/06/2025
 * Ultima alteracao.: 05/07/2025
+* Nome.............: ThreadNave.java
+* Funcao...........: Classe que controla todo a dinamica de movimento das Naves, por meio de uma thread. 
+* Essa classe define a imagem da Nave, a velocidade, o percurso pre-definido, o slider dela no ControllerTelaPrincipal, 
+* bem como controla os botoes que controlam a pausa e a retomada de cada thread da nave.
+*****************************************************************/
