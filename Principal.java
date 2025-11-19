b41d878ac96a4b26e2e18dd975cd49c8fc63add0
ItaloSLeao <italoleao.dev@gmail.com>
2025-11-19 14:47:33 -0300

    feat: atualiza classe principal

--- Principal.java
+++ Principal.java
@@ -3,3 +3,8 @@
 * Matricula........: 202410120
 * Inicio...........: 04/07/2025
 * Ultima alteracao.: 04/07/2025
+* Nome.............: Principal.java
+* Funcao...........: Realiza as importacoes necessarias para o pleno funcionamento proposto ao JavaFX no trabalho, inicia a aplicacao, 
+inicializa a tela principal do programa, carregando o respectivo arquivo fxml, e executa a aplicacao escolhida.
+*****************************************************************/
+
