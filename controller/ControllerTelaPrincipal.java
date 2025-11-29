61af153dfa27e71ed90c1df9b771b33419881b6a
ItaloSLeao <italoleao.dev@gmail.com>
2025-11-29 12:14:22 -0300

    feat: adiciona logica no controlador

--- controller/ControllerTelaPrincipal.java
+++ controller/ControllerTelaPrincipal.java
@@ -3,3 +3,8 @@
 * Matricula........: 202410120
 * Inicio...........: 01/07/2025
 * Ultima alteracao.: 05/07/2025
+* Nome.............: ControllerTelaPrincipal.java
+* Funcao...........: Controla a tela principal da aplicacao, onde, nesta: realiza a chamada das threads de movimentacao das naves; 
+* controla: as velocidades destas, dinamicamente; as funcoes de: fechar a aplicacao e resetar as posicoes e as velocidades, 
+* de pausar a movimentacao das naves, de visualizar o percurso das naves
+*****************************************************************/
