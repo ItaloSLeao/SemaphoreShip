1fc42ec5ac8da3985af9e8793520b036a5afce36
ItaloSLeao <italoleao.dev@gmail.com>
2025-12-03 03:18:20 -0300

    refactor: otimiza metodos do controlador

--- controller/ControllerTelaPrincipal.java
+++ controller/ControllerTelaPrincipal.java
@@ -13,3 +13,8 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.animation.ScaleTransition;
+import javafx.scene.control.Slider;
